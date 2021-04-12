a269295c9ebc0de1fed7d1ff6845fd8925bca0eb
Gabriel <gabrielmora2001@hotmail.com>
2021-04-11 20:33:49 -0500

    Revert "Revert "TXT añadido""
    This reverts commit 86907d063744151e1bafe70195642b3721ffaf94.

--- src/view/Main.java
+++ src/view/Main.java
@@ -1,6 +1,7 @@
 package view;
 
 
+import controller.Controller;
 import processing.core.PApplet;
 
 public class Main extends PApplet{
@@ -9,11 +10,12 @@ public class Main extends PApplet{
 		// TODO Auto-generated method stub
 		PApplet.main(Main.class.getName());
 	}
+	Controller controller;
 public void settings() {
 	size(700,700);
 }
 public void setup() {
-	
+	controller = new Controller(this);
 }
 public void draw() {
 	
